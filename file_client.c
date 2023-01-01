c85077e5b19eff00e93dffaac410b98bb775f4f9
Mertcan Karabaş <64467624+MertcanKarabas@users.noreply.github.com>
2023-01-01 21:32:29 +0300

    lastUpdates

--- file_client.c
+++ file_client.c
@@ -1,9 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <fcntl.h> //this is for open, close 
 
 int main () {
-
-
-
+    int fd;
+    while(1) {
+        char input[50];
+        scanf("%s", input);
+        printf("named_pipe yazma için açılıyor..\n");
+        fd = open("file_manager_named_pipe", O_WRONLY);
+        if (fd == -1) {
+            printf("named_pipe açılırken hata..\n");
+            return 1;
+        }
+        printf("named_pipe yazma için açıldı..\n");
+        if(write(fd, input, sizeof(input)) == -1) {
+            printf("yazılırken hata..\n");
+            return 2;
+        }
+        printf("%s", input);
+        
+        int i = 0;
+        if (i == 3) {
+            return 3;
+        }
+        i++;
+    }
+    close(fd);
     return 0;
 }
--- file_manager.c
+++ file_manager.c
@@ -10,16 +10,38 @@
 
 int main () {
 
+    printf("named_pipe oluşuyor..\n");
     if (mkfifo("file_manager_named_pipe", 0777) == -1) { //fifo is created or not.
         if (errno != EEXIST) { // If fifo is not created
         printf ("Could not create fifo file\n");
             return 1;
         }
     }
-    int fd = open("file_manager_named_pipe", O_WRONLY);    
-    if (fd == -1) {
-        return 2;
+    printf("named_pipe oluştu..\n");
+    int fd;
+    while(1){
+        char input[50];
+        char * fileList[10];
+        printf("named_pipe okuma için açılıyor...\n");
+        fd = open("file_manager_named_pipe", O_RDONLY);    
+        if (fd == -1) {
+            printf("named_pipe okuma için açılırken hata...\n");
+            return 2;
+        }
+        printf("named_pipe okuma için açıldı..\n");
+        if(read(fd, input, sizeof(input)) == -1) {
+            printf("okunurken hata..\n");
+            return 3;
+        }
+        printf("%s\n", input);
+        printf("named_pipe kapatılıyor..\n");
+        printf("named_pipe kapatıldı..\n");
+        int i = 0;
+        if(i == 3) {
+            return 4;
+        }
+        i++;
     }
-    
+    close(fd);
     return 0;
 }
