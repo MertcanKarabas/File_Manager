c34a9938c3091d7ebbdbce28ae0fe4f53f5a7ccd
Mertcan Karabaş <64467624+MertcanKarabas@users.noreply.github.com>
2023-01-03 21:57:08 +0300

    lastUpdates

--- file_client.c
+++ file_client.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <pthread.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -10,28 +10,57 @@
 
 int main () {
     int fd;
+    
     while(1) {
         char input[50];
-        scanf("%s", input);
-        printf("named_pipe yazma için açılıyor..\n");
+        char **words;
+        int i, j, k;
+        fgets(input, 50, stdin);  // Kullanıcıdan bir string alınır
+
+        words = (char**)malloc(sizeof(char*));
+        for (i = 0; i < 10; i++)
+            words[i] = (char*)malloc(sizeof(char));
+
+        input[strlen(input) -1 ] = '\0'; //enter karakterini sil.
         fd = open("file_manager_named_pipe", O_WRONLY);
         if (fd == -1) {
             printf("named_pipe açılırken hata..\n");
             return 1;
         }
+
         printf("named_pipe yazma için açıldı..\n");
         if(write(fd, input, sizeof(input)) == -1) {
             printf("yazılırken hata..\n");
             return 2;
         }
-        printf("%s", input);
-        
-        int i = 0;
-        if (i == 3) {
-            return 3;
+        printf("Comment sent!\n");
+
+        i = 0; j = 0;
+        int len = strlen(input);
+        // "command" array'i içerisinde kelime kelime ayrıştırılır
+        for (k = 0; k < len; k++) {
+            // Eğer kelime sonuna gelinmişse, j'yi sıfırla ve i'yi bir arttır
+            if (input[k] == ' ' || input[k] == '\0' || input[k] == '\n') {
+                words[i][j] = '\0';
+                i++;
+                j = 0;
+            } else {
+            // Aksi halde, kelimeyi "words" array'ine at
+                words[i][j] = input[k];
+                j++;
+            }
         }
-        i++;
+
+        if(strcmp(words[0], "Exit") == 0) {
+            printf("Çıkış yapılıyor..\n");
+            break;
+        }
+
+        free(words);       
     }
+    printf("pipe kapanıyor..\n");
     close(fd);
+    printf("pipe kapandı..\n");
+    printf("çıkış yapıldı...\n");
     return 0;
 }
